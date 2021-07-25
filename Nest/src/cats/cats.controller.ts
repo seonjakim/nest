81e1ef9d9e377087646c23f9adf1632cba522ae4
seonja <seonja.kim0@gmail.com>
2021-07-25 13:44:54 +0900

    nest project migration

---
+++ Nest/src/cats/cats.controller.ts
@@ -0,0 +1,55 @@
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Put,
+  UseFilters,
+  UseInterceptors,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+@UseInterceptors(SuccessInterceptor)
+@UseFilters(HttpExceptionFilter)
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCat() {
+    return 'all cat';
+  }
+
+  @Get(':id')
+  getOneCat(@Param('id', ParseIntPipe) param) {
+    console.log(param);
+    console.log(typeof param);
+    return 'one cat';
+  }
+
+  @Post()
+  createCat() {
+    return 'create cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return 'update';
+  }
+
+  @Delete(':id')
+  deleteCat() {
+    return 'delete';
+  }
+}
