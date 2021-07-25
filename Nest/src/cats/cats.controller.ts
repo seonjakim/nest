d970341a499369dacc50528731e5e5fc15cdd3d5
seonja <seonja.kim0@gmail.com>
2021-07-25 20:59:59 +0900

    create signUp api and save in DB

--- Nest/src/cats/cats.controller.ts
+++ Nest/src/cats/cats.controller.ts
@@ -8,11 +8,13 @@ import {
   Patch,
   Post,
   Put,
+  Body,
   UseFilters,
   UseInterceptors,
 } from '@nestjs/common';
 import { HttpExceptionFilter } from 'src/common/exceptions/http-exception.filter';
 import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
+import { CatRequestDto } from 'src/dto/cats.request.dto';
 import { CatsService } from './cats.service';
 
 @Controller('cats')
@@ -27,8 +29,8 @@ export class CatsController {
   }
 
   @Post()
-  async signUp() {
-    return 'create cat';
+  async signUp(@Body() body: CatRequestDto) {
+    return await this.catsService.signUp(body);
   }
 
   @Post('login')
--- Nest/src/cats/cats.service.ts
+++ Nest/src/cats/cats.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { CatRequestDto } from 'src/dto/cats.request.dto';
+import { Model } from 'mongoose';
+import { Cat } from './cats.schema';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+  constructor(@InjectModel(Cat.name) private readonly catModel: Model<Cat>) {}
+  async signUp(body: CatRequestDto) {
+    const { email, name, password } = body;
+    const isCatExist = await this.catModel.exists({ email });
+
+    if (isCatExist) {
+      /** both are return the same result */
+      // throw new HttpException('the email address is already exist', 403);
+      throw new UnauthorizedException('the email address is already exist');
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const cat = await this.catModel.create({
+      email,
+      name,
+      password: hashedPassword,
+    });
+
+    return cat.readOnlyData;
+  }
+}
